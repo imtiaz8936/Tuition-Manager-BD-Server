99dcc8dc0d2bf662ffc39fada511fb033dc65690
Imtiazul Hoque <imtiazulhoque8936@gmail.com>
2025-12-05 22:46:33 +0600

    project initialized with external package installation

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/server", (req, res) => {
+  res.send("tuition-manager-bd-server is runninggg gooood");
+});
+
+app.listen(port, () => {
+  console.log("tuition-manager-bd-server is running on port:", port);
+});
